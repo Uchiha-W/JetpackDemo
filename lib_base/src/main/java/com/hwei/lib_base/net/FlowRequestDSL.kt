6d022312b455a6bfc89febe02d26be7dede063a7
huangwei <hwei@33.cn>
2021-08-25 16:20:59 +0800

    轮询请求

--- lib_base/src/main/java/com/hwei/lib_base/net/FlowRequestDSL.kt
+++ lib_base/src/main/java/com/hwei/lib_base/net/FlowRequestDSL.kt
@@ -3,7 +3,11 @@ package com.hwei.lib_base.net
 import com.hwei.lib_base.base.BaseViewModel
 import com.hwei.lib_base.ktx.showToast
 import com.hwei.lib_base.net.base.Resource
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 import java.util.concurrent.CancellationException
 
@@ -62,6 +66,56 @@ fun <T> BaseViewModel.flowRequest(
             if (showLoading) {
                 showLoadingLiveData.value = false
             }
+        }
+    }
+}
+
+
+/**
+ * 轮询请求：
+ *
+//  lifecycleScope.launch {
+//    repeatOnLifecycle(Lifecycle.State.RESUMED) {
+//          viewModel.request()
+//        ).collect {
+//            todo
+//        }
+//    }
+//  }
+ */
+
+fun <T> BaseViewModel.pollFlowRequest(
+    delayMs: Long = 1000,
+    block: FlowRequestDSL<T>.() -> Unit
+): Flow<T> {
+    return callbackFlow {
+        val request = FlowRequestDSL<T>().apply(block)
+        while (true) {
+            delay(delayMs)
+            try {
+                request.onRequest?.invoke().apply {
+                    when (this) {
+                        is Resource.Success -> {
+                            request.onSuccess?.invoke(this.data())
+                            trySendBlocking(this.data())
+                        }
+                        is Resource.Error -> {
+                            request.onFailure?.invoke(this.error())
+                            showToast(this.error())
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                if (e !is CancellationException) {
+                    request.onFailure?.invoke(e.message.orEmpty())
+                    showToast(e.message.orEmpty())
+                }
+            } finally {
+                request.onComplete?.invoke()
+            }
+        }
+        awaitClose {
+            //poll = false
         }
     }
 }
